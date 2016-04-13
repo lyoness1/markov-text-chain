f8fa87b2a03f04a04c394d61a826224a999c284c
Allison Lyon <alyon@Allison-Lyons-Computer.local>
2016-04-12 21:16:35 -0700

    n-grams working!

--- markov.py
+++ markov.py
@@ -14,7 +14,7 @@ def open_and_read_file(file_path):
     return contents
 
 
-def make_chains(text_string, num_key_words = 2):
+def make_chains(text_string, num_key_words):
     """Takes input text as string; returns _dictionary_ of markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
@@ -26,44 +26,34 @@ def make_chains(text_string, num_key_words = 2):
         >>> make_chains("hi there mary hi there juanita")
         {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
     """
+    # intergerizes the input from str to int so it can be used in index math 
+    num_key_words = int(num_key_words)
+
     # initialize new dictionary
     chains = {}
 
     # split on whitespace delimeters
     words = text_string.split()
 
+    # iterates through words to create keys 
+    for word_index in range(len(words) - num_key_words):
+        # initializes an empty list to store the words that will create the key
+        key_word_list = []
+        # iterates over an index to add the correct number of words to the key list. 
+        # tuplizes key
+        for word_count in range(num_key_words):
+            key_word_list.append(words[word_index + word_count])
+            current_key = tuple(key_word_list)
 
-    # Iterates through words[], creates or appends word to key
-    for i in range(len(words)-num_key_words):
-        # makes a tuple num_of_words long
-        empty_list_of_words_for_key = []
-
-        for x in range(num_key_words):
-            empty_list_of_words_for_key.append(words[i])
-        
-        key_tuple = tuple(empty_list_of_words_for_key)
-        print key_tuple
-        # if key does not currently exist, append as follows into dict:
-        # (tuple_item_0th, tuple_item_1th) = [following word]
-        if key_tuple not in chains:
-            chains[key_tuple] = [words[i + num_key_words]]
-        else:
-            # if it does exist, only append to the value
-            # ex: [following word, other word that follows tuple combination]
-            chains[key_tuple].append(words[i + num_key_words])
-
-    # # Iterates through words[], creates or appends word to key
-    # for i in range(len(words)-num_key_words):
-    #     # if key does not currently exist, append as follows into dict:
-    #     # (tuple_item_0th, tuple_item_1th) = [following word]
-    #     if (words[i], words[i+1]) not in chains:
-    #         chains[(words[i], words[i+1])] = [words[i+2]]
-    #     else:
-    #         # if it does exist, only append to the value
-    #         # ex: [following word, other word that follows tuple combination]
-    #         chains[(words[i], words[i+1])].append(words[i+2])
+        # adds key to dictionary, or adds values to existing key
+        if current_key not in chains:
+            chains[current_key] = [words[word_index + num_key_words]]
+        else: 
+            chains[current_key].append(words[word_index + num_key_words])
 
     return chains
+
+
 
 
 def make_text(chains):
@@ -71,9 +61,11 @@ def make_text(chains):
 
     # initialize empty text for joining purposes
     text = ""
+
     # sets initial current key to a random key in dictionary
     current_key = choice(chains.keys())
 
+    # a string of punctuation that should end a line
     to_new_line_punctuation = "!-.:;?"
     
     # while we have not reached the condtion of:
@@ -88,11 +80,13 @@ def make_text(chains):
         # if word has no punctuation, add a space and the word to the text
         elif random_word[-1] not in to_new_line_punctuation:
             text += (random_word + " ")
-        # if the word has new-line-punctuation, add space + word + new line
+        # if the word ends with punctuation, have the word end the line. 
         else:
             text += (random_word + "\n")
-        # update the key to find the next word
-        current_key = (current_key[1], random_word)
+        # update the key to find the next word. Listizes, slices, adds random word, re-tuplizes. 
+        current_key_list = list(current_key)
+        current_key_list = current_key_list[1:] + [random_word]
+        current_key = tuple(current_key_list)
     
     # initializes an empty string to add to later
     formatted_text = ""
@@ -104,22 +98,26 @@ def make_text(chains):
     for line in text_lines:
         new_line = line.capitalize()
         formatted_text += new_line + "\n"
+        if formatted_text.count("\n") > 14:
+            break
 
 
     return formatted_text
 
 
+# determines what txt file to read as input text 
 input_path = sys.argv[1]
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
-# make_key_code(input_text, 4)
+# establishes the number of words to put into the dictionary keys. More = better text. 
+key_length = raw_input("How many words would you like to use in your Markov key chain? ")
 
 # Get a Markov chain
-chains = make_chains(input_text, 4)
+chains = make_chains(input_text, key_length)
 
 # Produce random text
 random_text = make_text(chains)
 
-print random_text
+print "\n", random_text
